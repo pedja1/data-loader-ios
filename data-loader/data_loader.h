75a021c1921deb87483bc6c056a652a5b70f9e4b
pedja1 <predragcokulov@gmail.com>
2015-12-21 08:47:03 +0100

    Initial Commit

---
+++ data-loader/data_loader.h
@@ -0,0 +1,13 @@
+//
+//  data_loader.h
+//  data-loader
+//
+//  Created by Predrag Cokulov on 12/21/15.
+//  Copyright © 2015 Predrag Cokulov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface data_loader : NSObject
+
+@end
---
+++ data-loader/data_loader.m
@@ -0,0 +1,13 @@
+//
+//  data_loader.m
+//  data-loader
+//
+//  Created by Predrag Cokulov on 12/21/15.
+//  Copyright © 2015 Predrag Cokulov. All rights reserved.
+//
+
+#import "data_loader.h"
+
+@implementation data_loader
+
+@end
